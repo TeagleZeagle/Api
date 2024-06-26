70dfa6d7f59781e3b90c00528dd3a5481ed6cbb6
Kaan Kani İnce <kkince@newmind.ai>
2024-06-26 16:48:01 +0300

    gereksiz yorumlar silindi

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,8 @@ const controller = require('../controller/controller');
 const router = new Router();
 
 router.get('/', controller.getUsers);
-router.post('/update-user', controller.updateUser); // hata
+router.post('/update-user', controller.updateUser);
 router.post('/create-user', controller.createUser);
-router.post('/deleteUser/:id', controller.deleteUser); // çalışıyor gibi gözüküyor çalışmıyor
+router.post('/deleteUser/:id', controller.deleteUser);
 
 module.exports = router;
